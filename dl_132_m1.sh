ad3eb45d45a882e411335a346d708cea62bb4bba
hfyan0@gmail.com <hfyan0@gmail.com>
2016-07-10 15:00:24 +0800

    WIP

---
+++ dl_132_m1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+BIN=/home/$(whoami)/Dropbox/dataENF/blmg/blmgItrdBarDownloader/target/blmg_itrd_bar_downloader-assembly-1.0-SNAPSHOT.jar
+M1ADJFOLDER=/home/$(whoami)/Dropbox/dataENF/blmg/m1_ohlc_adj/
+BLMGCOMMON=/home/$(whoami)/Dropbox/dataENF/blmg/common_path.sh
+
+source $BLMGCOMMON
+
+java -jar $BIN -s "HSI Index" -b 1 -o $M1ADJFOLDER/HSI".csv" -gmt 8
+
+# for sym in $SYMBOLLIST_HKIDX_M1
+# do
+#     java -jar $BIN -s "$sym Index" -b 1 -o $M1ADJFOLDER/$sym".csv" -gmt 8
+# done
+#
+# for sym in $SYMBOLLIST_CNIDX_M1
+# do
+#     java -jar $BIN -s "$sym Index" -b 1 -o $M1ADJFOLDER/$sym".csv" -gmt 8
+# done
+#
+# for sym in $SYMBOLLIST_SGIDX_M1
+# do
+#     java -jar $BIN -s "$sym Index" -b 1 -o $M1ADJFOLDER/$sym".csv" -gmt 8
+# done
+#
+# for sym in $SYMBOLLIST_JPIDX_M1
+# do
+#     java -jar $BIN -s "$sym Index" -b 1 -o $M1ADJFOLDER/$sym".csv" -gmt 9
+# done
+#
+# for sym in $SYMBOLLIST_GMT_M1
+# do
+#     java -jar $BIN -s "$sym Index" -b 1 -o $M1ADJFOLDER/$sym".csv" -gmt 0
+# done
